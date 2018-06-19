192ce97fb22f99a1a66d893186b2110a0bf044b9
Fabricio Dupont <fabriciodupont@hotmail.com>
2018-06-18 22:50:05 -0300

    Documentação Swagger

--- Holdprint.Api/Startup.cs
+++ Holdprint.Api/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+using System.IO;
 
 namespace Holdprint.Api
 {
@@ -34,6 +37,33 @@ namespace Holdprint.Api
             services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
             services.AddTransient(typeof(IRepositoryQuery<,>), typeof(RepositoryQuery<,>));
             services.AddTransient(typeof(IService<,>), typeof(Service<,>));
+
+            ConfigureSwagger(services);
+        }
+
+        public void ConfigureSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "Holdprint",
+                        Version = "v1",
+                        Description = "Holdprint API REST criada com o ASP.NET Core",
+                        Contact = new Contact
+                        {
+                            Name = "Fabricio Dupont",
+                            Url = "https://github.com/fabriciogd"
+                        }
+                    });
+
+                string caminhoAplicacao = PlatformServices.Default.Application.ApplicationBasePath;
+                string nomeAplicacao = PlatformServices.Default.Application.ApplicationName;
+                string caminhoXmlDoc = Path.Combine(caminhoAplicacao, $"{nomeAplicacao}.xml");
+
+                c.IncludeXmlComments(caminhoXmlDoc);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +75,13 @@ namespace Holdprint.Api
             }
 
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                    "Holdprint");
+            });
 
             AutoMapperConfiguration.Configure();
         }
